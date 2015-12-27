9d3d6b0a669e76b0189e2bd87511817e8be1b60a
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2015-12-28 02:17:33 +0800

    Add weblogic xml this is to make it work with oracle weblogic server

--- restsvc/src/main/java/hello/Application.java
+++ restsvc/src/main/java/hello/Application.java
@@ -1,12 +1,34 @@
-//package hello;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class Application {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(Application.class, args);
-//    }
-//}
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer implements WebApplicationInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        System.out.println("inside application builder ----------->>>>>>>>>>>>>>>>>>>>");
+
+        // Customize the application or call application.sources(...) to add sources
+        // Since our example is itself a @Configuration class we actually don't
+        // need to override this method.
+        return application;
+    }
+
+}

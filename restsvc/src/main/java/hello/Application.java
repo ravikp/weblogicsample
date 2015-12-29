6ceb9223c7e9911af0609fdec8e957dce9d900cc
Ravi Kumar Pasumarthy <ravikup@thoughtworks.com>
2015-12-29 11:36:49 +0800

    working in weblogic

--- restsvc/src/main/java/hello/Application.java
+++ restsvc/src/main/java/hello/Application.java
@@ -28,7 +28,9 @@ public class Application extends SpringBootServletInitializer implements WebAppl
         // Customize the application or call application.sources(...) to add sources
         // Since our example is itself a @Configuration class we actually don't
         // need to override this method.
-        return application;
+        //return application;
+        return application.sources(Application.class);
+
     }
 
 }
